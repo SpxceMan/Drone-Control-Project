d55e7a3d3baa7431b86d41dfdb7971308afe0304
oneSevenAR <akshat.richh174@gmail.com>
2025-02-06 10:33:17 +0530

    updated smoothing (pitch not added yet)

--- drone_testv0.2.1.py
+++ drone_testv0.2.1.py
@@ -7,8 +7,9 @@ import vgamepad as vg
 
 # Constants and initializations
 wCam, hCam = 640, 480
-alpha = 0.2  # Smoothing factor
-
+alpha_throttle = 0.15  # Smoothing factor
+alpha_roll = 0.4
+alpha_yaw = 0.1
 detector = htmod.handDetector(detectionCon=0.7)
 gamepad = vg.VX360Gamepad()
 
@@ -27,7 +28,7 @@ while True:
         break
 
     # Hand detection
-    frame = detector.findHands(frame)
+    frame = detector.findHands(frame, draw=False)
     lmList = detector.findPosition(frame, draw=False)
 
     if len(lmList) == 0:
@@ -44,24 +45,27 @@ while True:
     x2, y2 = lmList[8][1], lmList[8][2]    # Index finger
     x4, y4 = lmList[16][1], lmList[16][2]  # Ring finger
 
+    cv.line(frame, (x0, y0), (x3, y3), (255, 0, 0), 3)
+    cv.line(frame, (x2, y2), (x4, y4), (255, 0, 0), 3)
+
     # Throttle control (left joystick Y-axis)
     throttle_control = abs(y3 - y0)
-    throttle_min, throttle_max = 170, 400
+    throttle_min, throttle_max = 170, 450
     throttle_control = np.clip(throttle_control, throttle_min, throttle_max)
     
-    smooth_throttle = smooth_throttle * (1 - alpha) + throttle_control * alpha
+    smooth_throttle = smooth_throttle * (1 - alpha_throttle) + throttle_control * alpha_throttle
 
     # Roll control (now mapped to right joystick X-axis)
     roll_value = y4 - y2
     roll_min, roll_max = -210, 210
     roll_value = np.clip(roll_value, roll_min, roll_max)
-    smooth_roll = smooth_roll * (1 - alpha) + roll_value * alpha
+    smooth_roll = smooth_roll * (1 - alpha_roll) + roll_value * alpha_roll
 
     # Yaw control (right joystick Y-axis)
     yaw = x3 - x0
     yaw_min, yaw_max = -250, 250
     yaw = np.clip(yaw, yaw_min, yaw_max)
-    smooth_yaw = smooth_yaw * (1 - alpha) + yaw * alpha
+    smooth_yaw = smooth_yaw * (1 - alpha_yaw) + yaw * alpha_yaw
 
     # Map values to joystick ranges
     roll_right_stick_x = int(np.interp(smooth_roll, [roll_min, roll_max], [-32767, 32767]))  # Roll control
@@ -84,7 +88,7 @@ while True:
           f"Roll (Before Smooth): {roll_value} | Roll (After Smooth): {smooth_roll} | "
           f"Yaw (Before Smooth): {yaw} | Yaw (After Smooth): {smooth_yaw} | "
           f"Right Joystick X (Roll): {roll_right_stick_x} | Left Joystick X (Yaw): {yaw_left_stick_x} | "
-          f"Left Joystick Y (Throttle): {throttle_left_stick_y} | FPS: {int(fps)}")
+          f"Left Joystick Y (Throttle): {throttle_left_stick_y}")
 
     # Show frame
     frame = cv.resize(frame, (640, 480))
